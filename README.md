2d161c6ba0ebad6a746d1c6da1c8f50ff84fd82a
deenuy <deenuy@gmail.com>
2020-11-01 06:04:25 -0500

    refactored the code to add day planner functionality

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # UofT-day-planner-webapp
-Day planner is a simple calendar web application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+Day planner is a simple calendar web application that allows the user to save events for each hour of the day. This allows users to see their day at a glance, schedule time effectively, and improve productivity. The day planner app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+## Technology stack
+* HTML5, CSS
+* Javascript (jQuery, moment js, browser storage)
+
+Hosted Url: https://deenuy.github.io/UofT-day-planner-webapp/
+
+## Demo
+![Demo](assets/demo-day-planner.gif)
---
+++ assets/day-planner-scr.js
@@ -0,0 +1,229 @@
+// variables
+// create moment object for current moment
+var now = moment();
+
+var date = now;
+var dateIndex;
+var dateReadable;
+
+//set work hours, up to 10 hours
+var workHours=9;
+
+//empty object for when localStorage doesn't have data for a given date. 
+const emptyDateObject = {
+    hour0: "",
+    hour1: "",
+    hour2: "",
+    hour3: "",
+    hour4: "",
+    hour5: "",
+    hour6: "",
+    hour7: "",
+    hour8: "",
+    hour9: "",
+    hour10: "",
+};
+
+//Set current date 
+function setDate(){
+    dateIndex = date.format('YYYYMMDD');
+    dateReadable = date.format('MMMM Do YYYY');
+    console.log("Set current date: ", dateReadable);
+}
+
+// check url for params
+function checkURL(){
+    console.log('checkURL()');
+    // get URL parameters
+    var urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams);
+    // check url for date parameter
+    if (urlParams.has('date')){
+        //get date from URL param
+        dateParam = urlParams.get('date');
+        console.log("dateParam =", dateParam);
+        //create moment object from user date parameter
+        date= moment(dateParam, "YYYYMMDD");
+        //set date variables using new moment object
+        setDate();
+    }
+    // offset day's current hour for testing (does not change day)
+    if (urlParams.has('offset')){
+        //get test-hour parameter data
+        hourOffset = urlParams.get('offset');
+        parseInt(hourOffset);
+        console.log(now);
+        //offset now time by URL parameter value in hours (accepts negative)
+        now.add(hourOffset, 'h');
+        console.log(now);
+        console.log("time offset by", hourOffset, "hours");
+    }
+}
+
+// read saved planner information from localStorage
+function getPlanBrowserStorage(date) {
+    console.log("getPlanBrowserStorage()");
+    //if localStorage is empty, return an empty object
+    if ((localStorage.length===0) || (localStorage.getItem(dateIndex)===null)){
+        console.log("No data in localStorage, empty object returned", emptyDateObject);
+        return emptyDateObject;
+    //return data for current date it exists
+    } else {
+        console.log("Plans Object: ", JSON.parse(localStorage.getItem(dateIndex)));
+        return JSON.parse(localStorage.getItem(dateIndex));
+    }
+};
+
+// save all hours for current day to localStorage
+function saveAllHours() {
+    console.log('saveAllHours()');
+    // build an object from input field values
+    var dateObject = {
+        hour0: $("#inputHour0").val(),
+        hour1: $("#inputHour1").val(),
+        hour2: $("#inputHour2").val(),
+        hour3: $("#inputHour3").val(),
+        hour4: $("#inputHour4").val(),
+        hour5: $("#inputHour5").val(),
+        hour6: $("#inputHour6").val(),
+        hour7: $("#inputHour7").val(),
+        hour8: $("#inputHour8").val(),
+        hour9: $("#inputHour9").val(),
+        hour10: $("#inputHour10").val()
+    };
+    //save dateObject to localStorage
+    localStorage.setItem(dateIndex, JSON.stringify(dateObject));
+    console.log(localStorage);
+}
+
+// save only clicked hour to localStorage
+function saveHour() {
+    console.log('saveHour()');
+    //hour to update from event target
+    var hour = event.target.value;
+    //create string from event target to get input field value
+    var newRecordID="#inputHour"+hour[hour.length-1];
+    //get value from input field associated with specific button clicked
+    var newRecord=$(newRecordID).val();
+    //set localStorage key from value of clicked button 
+    var recordToUpdate=event.target.value;
+    //initialize object to write to localStorage
+    var dateObject;
+    //if local storage is not empty and has data for the current date, 
+    //build new record using data already in localStorage,
+    //disregarding the data on the page aside from the specific hour to be updated.
+    //this way only the record associated with the clicked button is updated.
+    if ((localStorage.length===0) || (localStorage.getItem(dateIndex)===null)){
+        console.log("No data in localStorage, empty object returned", emptyDateObject);
+        dateObject = {
+            hour0: $("#inputHour0").val(),
+            hour1: $("#inputHour1").val(),
+            hour2: $("#inputHour2").val(),
+            hour3: $("#inputHour3").val(),
+            hour4: $("#inputHour4").val(),
+            hour5: $("#inputHour5").val(),
+            hour6: $("#inputHour6").val(),
+            hour7: $("#inputHour7").val(),
+            hour8: $("#inputHour8").val(),
+            hour9: $("#inputHour9").val(),
+            hour10: $("#inputHour10").val()
+        };
+    //return data for current date it exists
+    } else {
+        dateObject = JSON.parse(localStorage.getItem(dateIndex));
+    }
+    console.log("dateObject",dateObject);
+    //add new record to the date object
+    dateObject[recordToUpdate]=newRecord;
+    console.log("dateObject newRecord",dateObject);
+    //write the object containing the new record to localStorage
+    localStorage.setItem(dateIndex, JSON.stringify(dateObject));
+}
+
+// render page for date provided
+function renderPage(){
+    // read plans for localStorage
+    var plans = getPlanBrowserStorage(date);
+    var workDayHour = moment(dateIndex+"T09");
+    console.log(workDayHour);
+
+    $(".set-now").text(dateReadable);
+
+    // determine if time slot is in past present or future, set CSS class
+    var relTime=""; // past, present, future
+    for (var i=0;i<workHours;i++){
+
+        if (parseInt(workDayHour.format('HH')) < parseInt(now.format('HH'))) {
+            relTime="past";
+        } else if (parseInt(workDayHour.format('HH'))===parseInt(now.format('HH'))) {
+            relTime="present";
+        } else if (parseInt(workDayHour.format('HH'))>parseInt(now.format('HH'))) {
+            relTime="future";
+        }
+
+        console.log(parseInt(workDayHour.format('HH')), parseInt(now.format('HH')), relTime);    
+
+        //generate li elements for each hour of work day with relTime classes
+
+        $("#calendar").append(
+            `
+            <li class="hour ${relTime}" data-hour=${i}>
+                <form>
+                    <label>${workDayHour.format('h a')}</label>
+                    <input type="text" id="inputHour${i}" value="${plans["hour"+i]}">
+                    <button type="submit" class="save" value="hour${i}">&#128190</button>
+                </form>
+            </li>
+            `
+            );
+
+        workDayHour.add(1, 'hour');  
+    };
+}
+
+// clear input fields for current day
+function clearDate(){
+    $("#inputHour0").val('');
+    $("#inputHour1").val('');
+    $("#inputHour2").val('');
+    $("#inputHour3").val('');
+    $("#inputHour4").val('');
+    $("#inputHour5").val('');
+    $("#inputHour6").val('');
+    $("#inputHour7").val('');
+    $("#inputHour8").val('');
+    $("#inputHour9").val('');
+    $("#inputHour10").val('');
+}
+
+// event listeners
+function addEventListeners() {
+    // save hour
+    $("button.save").on("click", function(e){
+        e.preventDefault();
+        // save hour to localStorage
+        saveHour();
+    });
+    // clear plans
+    $("#clearDay").on("click", function(){
+        //clear current day's tasks on page
+        clearDate();
+        //write tasks to localStoge
+        saveAllHours()
+    });
+    // save all hours
+    $("#saveAll").on("click", function(){
+        // save plans to localStorage
+        saveAllHours();
+    });
+}
+
+//main function
+function runApp(){
+    setDate();
+    checkURL();
+    renderPage();
+    addEventListeners();
+}
+
+runApp();
